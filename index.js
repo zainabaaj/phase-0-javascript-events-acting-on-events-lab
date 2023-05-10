2fc9a9acc1a2abdc19ff279214965cfaf15a0d4d
Zeyna Baaj <zainabaaj@gmail.com>
2023-05-10 14:41:42 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowLeft") {
+        moveDodgerLeft();
+    }
+    if (event.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+});
+
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    dodger.style.left = `${left + 1}px`;
+
+}
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    dodger.style.left = `${left - 1}px`;
+
+}
